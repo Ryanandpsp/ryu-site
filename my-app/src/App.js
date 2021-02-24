dc6bf97cc2f4cfc19b8c6762a257096955407444
Ryanandpsp <r_liu_321@hotmail.com>
2021-02-23 22:34:49 -0500

    learned how to do some stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
+import AButton from "./Components/button"
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <AButton name = "test button"></AButton>
     </div>
   );
 }
---
+++ my-app/src/Components/button.js
@@ -0,0 +1,7 @@
+import "./styles.css"
+
+function AButton(props) {
+    return <button className = "button">{props.name}</button>;
+  }
+
+export default AButton;
